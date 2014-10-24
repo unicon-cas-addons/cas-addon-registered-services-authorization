4194c3c83fd36fc6f82b2e7bd6b16f0f18133d09
Dmitriy Kopylenko <dima767@gmail.com>
2014-10-24 11:25:48 -0400

    @Conditional beans configuration

--- src/main/groovy/net/unicon/cas/addon/registeredservices/authorization/config/ServicesAuthorizationConfig.groovy
+++ src/main/groovy/net/unicon/cas/addon/registeredservices/authorization/config/ServicesAuthorizationConfig.groovy
@@ -1,13 +1,14 @@
 package net.unicon.cas.addon.registeredservices.authorization.config
 
-
 import net.unicon.cas.addon.registeredservices.authorization.DefaultRegisteredServiceAuthorizer
 import net.unicon.cas.addon.registeredservices.authorization.RegisteredServiceAuthorizer
 import net.unicon.cas.addon.registeredservices.authorization.webflow.ServiceAuthorizationAction
 import org.jasig.cas.services.ServicesManager
 import org.jasig.cas.ticket.registry.TicketRegistry
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 
 /**
  * Spring Application Context configuration for service authorization addon beans.
@@ -16,15 +17,18 @@ import org.springframework.context.annotation.Configuration
  * @since 1.0.0
  */
 @Configuration
+@Order //to make sure the lowest precedence order of this config processing is set to let all the other beans in the ctx be defined first
 class ServicesAuthorizationConfig {
 
     @Bean
-    ServiceAuthorizationAction serviceAuthorizationAction(ServicesManager servicesManager, TicketRegistry ticketRegistry) {
-        new ServiceAuthorizationAction(servicesManager, ticketRegistry, registeredServiceAuthorizer())
+    ServiceAuthorizationAction serviceAuthorizationAction(ServicesManager servicesManager, TicketRegistry ticketRegistry,
+                                                          RegisteredServiceAuthorizer authorizer) {
+        new ServiceAuthorizationAction(servicesManager, ticketRegistry, authorizer)
     }
 
     @Bean
-    public RegisteredServiceAuthorizer registeredServiceAuthorizer() {
+    @ConditionalOnMissingBean(RegisteredServiceAuthorizer)
+    public RegisteredServiceAuthorizer defaultAuthorizer() {
         new DefaultRegisteredServiceAuthorizer()
     }
 }
